01d53d950f95bff89f947093414aeceb8a0805d0
0-11mohamed <ekmohamed2006@gmail.com>
2025-09-23 17:00:56 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("monCanvas");
+const result = { best_score: 0, score: 0};
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = 'black';
+ctx.fillRect(10,10,100,100);
